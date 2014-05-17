e3b463646ef6a79794364a223f1686c76f4eab49
vinit Kumar <vinit.kumar@changer.nl>
2014-05-17 12:12:24 +0530

    remove greater sign because if makes the code invalid

--- cd.lisp
+++ cd.lisp
@@ -1,18 +1,18 @@
-> (defun make-cd (title artist rating ripped)
+ (defun make-cd (title artist rating ripped)
   (list :title title :artist artist :rating rating :ripped ripped))
 
 ;; Running this will create a new entry in the table
-> (make-cd "Roses" "Kathe Mattea" 7 t)
+ (make-cd "Roses" "Kathe Mattea" 7 t)
 
 
 ;; define a global variable db
-> (defvar *db* nil)
+ (defvar *db* nil)
 
-> (defun add-record (cd) (push cd *db*))
+ (defun add-record (cd) (push cd *db*))
 
 ;; add new entries like this from so on.
-> (add-record (make-cd "Rose" "Dad" 7 t))
+ (add-record (make-cd "Rose" "Dad" 7 t))
 
 ;; This command will show the current db
-> *db*
+ *db*
 
